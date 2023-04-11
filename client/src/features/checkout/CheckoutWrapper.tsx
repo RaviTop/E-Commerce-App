899ea35df6e57879f4fc6419692c7e7141b33511
Raviraj Kamalakar <ravirajkamalakar97@gmail.com>
2023-04-11 15:50:03 +0530

    first commit

---
+++ client/src/features/checkout/CheckoutWrapper.tsx
@@ -0,0 +1,30 @@
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutPage from "./CheckoutPage";
+import { loadStripe } from "@stripe/stripe-js";
+import { useState, useEffect } from "react";
+import agent from "../../app/api/agent";
+import { useAppDispatch } from "../../app/store/configureStore";
+import { setBasket } from "../basket/basketSlice";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+
+const stripePromise = loadStripe('pk_test_51Mj45RSGrPmVCPkWUAf5pOGA8CXGlbTfKR98fxjg1PSjRp7sYjm8PeS9AFR7ufqStiBdAYHKSeEzlbjdW37lZEIa00wUufJ7Zc');
+
+export default function CheckoutWrapper() {
+    const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        agent.Payments.createPaymentIntent()
+            .then(response => dispatch(setBasket(response)))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }, [dispatch]);
+
+    if (loading) return <LoadingComponent message='Loading checkout' />
+
+    return (
+        <Elements stripe={stripePromise}>
+            <CheckoutPage />
+        </Elements>
+    )
+} 
